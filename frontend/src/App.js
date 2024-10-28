a7efbb2abd75d94fcb10946cdee4c1fb3cdaaf33
ayushsharma-1 <ayushsharma18001@gmail.com>
2024-10-28 17:04:39 +0530

    Css update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import WeatherDisplay from './components/WeatherDisplay';
 import './App.css';
-
+import bgVideo from './icons/background.mp4';
 const App = () => {
   const [weatherData, setWeatherData] = useState([]); // Initialize as an empty array
   const [error, setError] = useState('');
@@ -27,9 +27,15 @@ const App = () => {
 
   return (
     <div className="App">
+      <video autoPlay loop muted className="bg-video">
+        <source src={bgVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
       <h1>Weather Monitoring System</h1>
       {error && <div className="error">{error}</div>}
       <WeatherDisplay weatherData={weatherData} />
+    </div>
     </div>
   );
 };
